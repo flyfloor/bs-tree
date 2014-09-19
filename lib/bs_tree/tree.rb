beb1fa5faf3f6a5f023c4d78424635705bb00a19
jerryshew <jerryshew@gmail.com>
2014-09-19 22:59:40 +0800

    tab size

--- lib/bs_tree/tree.rb
+++ lib/bs_tree/tree.rb
@@ -1,31 +1,31 @@
 class BsTree::Tree
 
-	def self.traval(type=:dlr, tree)
-		@@sequnce = []
-		if respond_to?("#{type}".to_sym) then	send("#{type}".to_sym, tree)
-		else
-			raise "#{type} kind of traval does not exist"
-		end
-	end
+  def self.traval(type=:dlr, tree)
+    @@sequnce = []
+    if respond_to?("#{type}".to_sym) then send("#{type}".to_sym, tree)
+    else
+      raise "#{type} kind of traval does not exist"
+    end
+  end
 
-	def self.sequnce
-		@@sequnce
-	end
+  def self.sequnce
+    @@sequnce
+  end
 
 
-	private
-		#DLR traval 
-		def self.dlr tree
-		 	@@sequnce << tree.value
-		  send(:dlr, tree.left) if tree.left
-		  send(:dlr, tree.right) if tree.right
-		end
+  private
+    #DLR traval 
+    def self.dlr tree
+      @@sequnce << tree.value
+      send(:dlr, tree.left) if tree.left
+      send(:dlr, tree.right) if tree.right
+    end
 
-		#LRD traval 
-		def self.lrd tree
-		  send(:lrd, tree.left) if tree.left    
-		  send(:lrd, tree.right) if tree.right
-		  @@sequnce << tree.value
-		end
+    #LRD traval 
+    def self.lrd tree
+      send(:lrd, tree.left) if tree.left    
+      send(:lrd, tree.right) if tree.right
+      @@sequnce << tree.value
+    end
 
 end
